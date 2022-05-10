3a6b8dbb8a4de32abf9c69488c1cbe0cb1652d11
AnhDuc2k <khungkhip@gmail.com>
2022-05-10 22:30:02 +0700

    update api blog

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
+import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-
 app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
 
 mongoose
   .connect(
