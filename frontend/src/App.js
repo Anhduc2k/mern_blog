0aef60ed356c3904926ce3e9168eec11d68c6a25
AnhDuc2k <khungkhip@gmail.com>
2022-05-11 21:46:17 +0700

    create authSlice for loggin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { AddBlog } from "./components/AddBlog";
 import { Auth } from "./components/Auth";
@@ -7,6 +8,7 @@ import { Header } from "./components/Header";
 import { UserBlogs } from "./components/UserBlogs";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
   return (
     <>
       <header>
@@ -14,11 +16,16 @@ function App() {
       </header>
       <main>
         <Routes>
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/blogs/add" element={<AddBlog />} />
-          <Route path="/myBlogs" element={<UserBlogs />} />
-          <Route path="/myBlogs/:id" element={<BlogDetail />} />{" "}
+          {!isLoggedIn ? (
+            <Route path="/auth" element={<Auth />} />
+          ) : (
+            <>
+              <Route path="/blogs" element={<Blogs />} />
+              <Route path="/blogs/add" element={<AddBlog />} />
+              <Route path="/myBlogs" element={<UserBlogs />} />
+              <Route path="/myBlogs/:id" element={<BlogDetail />} />{" "}
+            </>
+          )}
         </Routes>
       </main>
     </>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,20 @@
-import React from "react";
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store";
 export const Header = () => {
+  const dispath = useDispatch();
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  const [value, setValue] = useState();
   return (
     <AppBar
       position="sticky"
@@ -10,18 +24,56 @@ export const Header = () => {
       }}
     >
       <Toolbar>
-        <Typography variant="h4">Blog app</Typography>
-        <Box display="flex" marginLeft={"auto"}>
-          <Button
-            color="warning"
-            sx={{ margin: 1, borderRadius: 10 }}
-            variant="contained"
-          >
-            Login
-          </Button>
-          <Button sx={{ margin: 1, borderRadius: 10 }} variant="contained">
-            Signup
-          </Button>
+        <Typography variant="h4">Lionel Messi</Typography>
+        {isLoggedIn && (
+          <Box display="flex" marginLeft={"auto"} marginRight="auto">
+            <Tabs
+              textColor="inherit"
+              value={value}
+              onChange={(e, val) => setValue(val)}
+            >
+              <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
+              <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
+              <Tab LinkComponent={Link} to="/blogs/add" label="Add Blog" />
+            </Tabs>
+          </Box>
+        )}
+        <Box display="flex" marginLeft="auto">
+          {!isLoggedIn && (
+            <>
+              {" "}
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+                color="warning"
+              >
+                Đăng nhập
+              </Button>
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+                color="warning"
+              >
+                Đăng kí
+              </Button>
+            </>
+          )}
+          {isLoggedIn && (
+            <Button
+              onClick={() => dispath(authActions.logout())}
+              LinkComponent={Link}
+              to="/auth"
+              variant="contained"
+              sx={{ margin: 1, borderRadius: 10 }}
+              color="warning"
+            >
+              Đăng xuất
+            </Button>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
