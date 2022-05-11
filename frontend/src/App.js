9d262b9cb6be1109865af02dd6a43f46ca861c2d
AnhDuc2k <khungkhip@gmail.com>
2022-05-11 21:18:27 +0700

    create router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import { AddBlog } from "./components/AddBlog";
+import { Auth } from "./components/Auth";
+import { BlogDetail } from "./components/BlogDetail";
+import { Blogs } from "./components/Blogs";
+import { Header } from "./components/Header";
+import { UserBlogs } from "./components/UserBlogs";
+
 function App() {
-  return <div>aaa</div>;
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/add" element={<AddBlog />} />
+          <Route path="/myBlogs" element={<UserBlogs />} />
+          <Route path="/myBlogs/:id" element={<BlogDetail />} />{" "}
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,29 @@
 import React from "react";
-
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <AppBar
+      position="sticky"
+      sx={{
+        background:
+          "linear-gradient(90deg, rgba(58,75,180,1) 2%, rgba(116,49,110,1) 36%, rgba(2,0,161,1) 73%, rgba(69,92,252,1) 100%)",
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h4">Blog app</Typography>
+        <Box display="flex" marginLeft={"auto"}>
+          <Button
+            color="warning"
+            sx={{ margin: 1, borderRadius: 10 }}
+            variant="contained"
+          >
+            Login
+          </Button>
+          <Button sx={{ margin: 1, borderRadius: 10 }} variant="contained">
+            Signup
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
