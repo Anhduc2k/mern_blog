71f4b6abb6fe0e57dd00914da169259053758d97
AnhDuc2k <khungkhip@gmail.com>
2022-05-11 22:07:56 +0700

    fix canot acess before  initalization

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,8 @@ import mongoose from "mongoose";
 import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 
-app.use(cors());
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
