87ad7e22b8529311d209f32473473f17cf1d727e
AnhDuc2k <khungkhip@gmail.com>
2022-05-11 22:01:56 +0700

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
+import mongoose from "mongoose";
 import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 
-const app = express();
 app.use(cors());
+const app = express();
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
